c2450b0f2b3d5262a761db7b876fa35422fe2c5e
Vinaychandran Pondenkandath <vinaychandran.pondenkandath@unifr.ch>
2017-11-15 11:29:10 +0900

    Code to process dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,106 @@
-# -*- coding: utf-8 -*-
 import os
-import click
-import logging
-from dotenv import find_dotenv, load_dotenv
+import argparse
+import numpy as np
+import cv2
+import progressbar
+
+IMG_EXTENSIONS = [
+    '.jpg', '.JPG', '.jpeg', '.JPEG',
+    '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
+]
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+
+def get_list_images(dir):
+    images = []
+    for root, _, fnames in sorted(os.walk(dir)):
+        for fname in fnames:
+            if is_image_file(fname):
+                path = os.path.join(root, fname)
+                images.append(path)
+    return images
+
+
+def process_gt(gt_img_path):
+    gt_img = cv2.imread(gt_img_path)
+    gt_img_proc = np.zeros((gt_img.shape[0], gt_img.shape[1]), dtype=np.uint8)
+
+    transform_pairs = [[1,0], # Background
+                       [2,1], # Comment
+                       [4,2], # Decoration
+                       [8,4], # Text
+                       [6, 3],  # Comment + Decoration
+                       [10,5], # Comment + Text
+                       [12,6], # Decoration + Text
+                       [14,7]] # Comment + Decoration + Text
+
+    # Find Border Pixels and set them to background
+    locs = np.where(gt_img[:,:,2] == 128)
+    gt_img_proc[locs[0], locs[1]] = 0
+
+    for gt_label, our_label in transform_pairs:
+        locs = np.where(gt_img[:,:,0] == gt_label)
+        gt_img_proc[locs[0], locs[1]] = our_label
+
+    return gt_img_proc
+
+
+def get_filename_no_ext(name):
+    return name.split('/')[-1].split('.')[0]
+
+
+def make_dataset(data_folder, ground_truth, save_path, resize):
+    data_list = get_list_images(data_folder)
+    gt_list = get_list_images(ground_truth)
+    try:
+        assert len(data_list) == len(gt_list)
+    except AssertionError:
+        print("#Images in data_folder does not correspond to #images in ground_truth_folder")
+    bar = progressbar.ProgressBar(max_value=len(data_list))
+    train = []
+    val = []
+    test = []
+    for i, (img_path, gt_img_path) in bar(enumerate(zip(sorted(data_list), sorted(gt_list)))):
+        try:
+            assert get_filename_no_ext(img_path) == get_filename_no_ext(gt_img_path)
+        except AssertionError:
+            print("Filenames do not correspond: {} \t {}".format(img_path, gt_img_path))
+
+
+        category = img_path.split('/')[-2]
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if resize is not None:
+            img = cv2.resize(img, (0,0), fx=resize, fy=resize, interpolation=cv2.INTER_NEAREST)
+        gt_img = process_gt(gt_img_path)
+        if resize is not None:
+            gt_img = cv2.resize(gt_img, (0,0), fx=resize, fy=resize, interpolation=cv2.INTER_NEAREST)
+        if category == 'training':
+            train.append([img, gt_img, img_path, gt_img_path])
+        elif category == 'validation':
+            val.append([img, gt_img, img_path, gt_img_path])
+        elif category == 'test':
+            test.append([img, gt_img, img_path, gt_img_path])
+        else:
+            print("Unknown category!")
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    np.save(os.path.join(save_path, 'train.npy'), train)
+    np.save(os.path.join(save_path, 'val.npy'), val)
+    np.save(os.path.join(save_path, 'public-test.npy'), test)
+
+def main(opts):
+    make_dataset(opts.data_folder, opts.ground_truth, opts.save_path, opts.resize)
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_folder', help='Path to root')
+    parser.add_argument('--ground_truth', help='Path to corresponding ground truth')
+    parser.add_argument('--save_path', help='Path to folder where train/val/test splits will be saved.')
+    parser.add_argument('--resize', type=float, help='Factor by which to resize images', default=None)
+    args = parser.parse_args()
+    main(args)
